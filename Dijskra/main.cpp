31a0b9c6fad841500115344a8e51d61e8a4ad682
Dominik Plisek <dominik.plisek@plech.org>
2013-05-21 22:50:26 +0200

    updated tester

--- Dijskra/main.cpp
+++ Dijskra/main.cpp
@@ -8,11 +8,25 @@
 
 #include <iostream>
 
+using namespace std;
+
+
+#ifndef __PROGTEST__
+#include "CGraph.h"
+#include "t_edge.h"
+#include "t_node.h"
+#endif
+
+void Dijskra(CGraph& g, const int startIndex) {
+    
+}
+
+#ifndef __PROGTEST__
 int main(int argc, const char * argv[])
 {
-
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
-
+#endif
